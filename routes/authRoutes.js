b70757ce62f1ef67f44ecb2a0c4517eab6281111
Bo-T19 <santiago.b.segura@gmail.com>
2025-05-21 09:25:46 -0500

    Fixed auth bugs

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authControllers');
+const authControllers = require('../controllers/authControllers');
 
 // Start the authentication flow
-router.get('/auth', authController.initiateAuth);
+router.get('/auth', authControllers.initiateAuth);
 
 // Callback after Autodesk authentication
-router.get('/callback', authController.handleCallback);
+router.get('/callback', authControllers.handleCallback);
 
 // Protected test endpoint
-router.get('/api/test', authController.ensureValidToken, authController.testToken);
+router.get('/api/test', authControllers.ensureValidToken, authControllers.testToken);
 
 // Logout
-router.get('/logout', authController.logout);
+router.get('/logout', authControllers.logout);
 
 module.exports = router;
---
+++ routes/issuesRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const authControllers = require('../controllers/authControllers');
+
+// Get the issues of a specific project
+router.get('/api/issues/:id', authController.ensureValidToken);
+
+
+module.exports = router;
