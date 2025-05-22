b08851eefdc10511e9cbb90fdc9789c26d481207
Bo-T19 <santiago.b.segura@gmail.com>
2025-05-22 15:26:30 -0500

    Fetching ACC users updated

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -8,24 +8,37 @@ dotenv.config();
 const accountId = process.env.ACCOUNT_ID
 
 exports.getAutodeskUsers = async (req, res) => {
-
+    const users = [];
+    let offset = 0;
+    const limit = 100; // Max users number per page
     try {
         //Get the two legged token
         const token = req.tokenInfo.access_token;
 
-        // APS endpoint that gets the user of a specific account
-        const url = `https://developer.api.autodesk.com/hq/v1/accounts/${accountId}/users`;
+        while (true) {
+            const response = await axios.get(`https://developer.api.autodesk.com/hq/v1/accounts/${accountId}/users`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+                params: {
+                    limit,
+                    offset
+                }
+            });
 
-        const response = await axios.get(url, {
-            headers: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/json'
+            const data = response.data;
+            if (data.length === 0) {
+                break;
             }
-        });
+
+            users.push(...data);
+            offset += limit;
+        }
+
         res.json({
             success: true,
-            data: response.data || [],
-            total: response.data?.length || 0
+            data: users || [],
+            total: users.length || 0
         });
     } catch (error) {
         console.error('Error al obtener usuarios de Autodesk:', error.response?.data || error.message);
